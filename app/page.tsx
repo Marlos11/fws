18a5e878005103d1aa860fe4f32f3c1d8f146863
Marlos11 <marlos-gama@hotmail.com>
2024-04-30 21:18:28 -0300

    feat: add config husky and eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+import Image from "next/image";
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search-inputs";
 
 const Home = () => {
   return (
     <>
-      <Header />;
+      <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/Banner.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain "
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
